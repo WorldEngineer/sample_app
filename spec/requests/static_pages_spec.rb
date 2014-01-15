23858ebc52ec1c1a2e3bca07d06242e83fe8e6a7
world_engineer <alakazam442@yahoo.com>
2014-01-14 20:32:40 -0500

    Finished Static Pages with tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,12 +2,19 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
   describe "Home page" do
      
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
+    end
+  
+    it "should have the title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   
@@ -17,6 +24,11 @@ describe "StaticPages" do
      visit '/static_pages/help'
      expect(page).to have_content('Help') 
     end
+    
+    it "should have the title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+    end
   end
   
   describe "About page" do
@@ -24,6 +36,25 @@ describe "StaticPages" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+    
+    it "should have the title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+  
+  describe "Contact page" do
+     
+    it "should have the content 'Contact'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+  
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
